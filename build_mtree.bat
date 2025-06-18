b2dcb7284cbd5e96783c77d79a10cd011a242160
Ethan Porcaro <ethan@varrett.com>
2025-06-18 07:22:13 -0600

    Initial bump to python3.11

--- build_mtree.bat
+++ build_mtree.bat
@@ -1,6 +1,6 @@
-py -3.9 m_tree/install.py
+py -3.11 m_tree/install.py
 
-COPY ".\m_tree\binaries\Release\m_tree.cp39-win_amd64.pyd" "./m_tree.cp39-win_amd64.pyd"
+COPY ".\m_tree\binaries\Release\m_tree.cp311-win_amd64.pyd" "./m_tree.cp311-win_amd64.pyd"
 
 PAUSE
 
